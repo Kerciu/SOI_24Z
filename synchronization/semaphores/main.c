4f6faa8dc91bef1fe5834b707ebcce1ea068f752
Kerciu <kerciuuu@gmail.com>
2024-12-05 17:20:48 +0100

    created producent and consument functions that work on buffers and are synchronized by empty and full semaphores

--- synchronization/semaphores/main.c
+++ synchronization/semaphores/main.c
@@ -2,15 +2,46 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
-
 #include "buf.h"
+
+#define ITERS M
+
 
 Buffer buffer = { .content = 0, .head = 0, .tail = 0};
 
 sem_t empty;
 sem_t full;
-
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+void producent(void* arg)
+{
+    for (int i = 0; i < ITERS; ++i)
+    {
+        sem_wait(&empty);
+        pthread_mutex_lock(&mutex);
+
+        put(i, &buffer);
+
+        pthread_spin_unlock(&mutex);
+        sem_post(&full);
+    }
+}
+
+void consument(void* arg)
+{
+    for (int i = 0; i < ITERS; ++i)
+    {
+        sem_wait(&full);
+        pthread_mutex_lock(&mutex);
+
+        int thing = get(&buffer);
+
+        pthread_spin_unlock(&mutex);
+        sem_post(&empty);
+    }
+}
 
 int main(int argc, char** argv)
 {
